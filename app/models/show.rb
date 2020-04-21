82cafe17058332d277e8c70bc9d2bd3dba8e9a4d
Derric Fragosa <fderric@gmail.com>
2020-04-21 06:26:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,31 @@
 class Show < ActiveRecord::Base 
+  def self.highest_rating
+  show = Show.maximum(:rating)
+end
+
+def most_popular_show
+  
+end
+
+def lowest_rating
+  
+end
+
+def least_popular_show
+  
+end
+
+def ratings_sun
+  
+end
+
+def popular_shows
+  
+end
+
+def shows_by_alphabetical_order
+
+end
+
   
 end
